cdecb0e5f753bda456355095df259fdb4ea07a77
chen-wenjun <44269948+chen-wenjun@users.noreply.github.com>
2018-11-18 23:12:55 -0500

    Added Core Console App
    Added Core Console App

---
+++ CoreConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
